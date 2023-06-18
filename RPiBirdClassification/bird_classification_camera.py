a10152de81d8b4cba04c7581429f495b9a793a68
Coldice54 <alex@berkaloff.com>
2023-06-17 20:15:10 -0700

    Added comments to bird_classification_camera

--- RPiBirdClassification/bird_classification_camera.py
+++ RPiBirdClassification/bird_classification_camera.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+###################################################################
+# Main Bird Classification Runtime
+# Alexander Berkaloff 2023
+################################################################
+# Parts Based on the Google AIY Vision Toolkit
+# Copyright 2017 Google Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import contextlib
@@ -32,13 +49,21 @@ def CameraPreview(camera, enabled):
     finally:
         if enabled:
             camera.stop_preview()
-
+            
+# getBestClassFromSpan
+# Aggregates the scores of each class over
+# the last frameCount frames and
+# returns the class with the highest score.
 def getBestClassFromSpan(allFrames):
     classesInAllFrames = {}
+    # print("getBestClassFromSpan:")
+    print("==")
     for i,classes in enumerate(allFrames):
-       for c in classes:
+        #print("frame:"+str(i) ) #+"count:"+str(len(classes)))
+        for c in classes:
             className = c[0]
             score = c[1]
+            # print("className:"+className+" score:"+str(score))
             if className in classesInAllFrames: #if the class is already in the dict
                 classesInAllFrames[className] += score #add the probability to the existing probability
             else:
@@ -46,31 +71,40 @@ def getBestClassFromSpan(allFrames):
     #get the max class in the window
     if (len(classesInAllFrames) == 0):
         return (None,0)
-    print("========allFramesResult===========:")
-    for classes in classesInAllFrames:
-        print("classes:"+classes+" score:"+str(classesInAllFrames[classes]))
+    print("==Merged over "+str(len(allFrames))+"frames:")
+    for c in classesInAllFrames:
+        print("class:"+c+" score:"+str(classesInAllFrames[c]))
     topClass = max(classesInAllFrames, key=classesInAllFrames.get)
     return (topClass,classesInAllFrames[topClass])
 
-def isNewVisit(classesInCurrentFrame, multiFrameThreshold):
+# isNewVisit
+# checks whether we are seeing a new bird
+def isNewVisit(classesInCurrentFrame,multiFrameThreshold):
     global lastClassName
+    
     if (len(allFrames) == settings["frameCount"]):
         allFrames.pop(0)
     allFrames.append(classesInCurrentFrame)
     if (len(allFrames) == settings["frameCount"]):
         className,score = getBestClassFromSpan(allFrames)
-        if (className == None or  className == 'background'):
+        if (className == None or className == 'background'):
             return (None,0)
         elif score > multiFrameThreshold:
             if className == lastClassName:
                 print('Visit detected but same bird: ' + className)
                 return (None,0)
-            lastClassName = className
+            lastClassName = className #now we can save after we have tested 
+            #empty the allFrames list to restart visit detection from scratch
+            print("last frame-count:"+str(len(allFrames[frameCount-1])))
+            for c in allFrames[frameCount-1]:
+                print("className:"+c[0]+" score:"+str(c[1]))
+            print('New visit detected: ' + className)
             allFrames.clear()
             return (className,score)
     return (None,0)
 
-
+# checkSettings
+# checks whether settings have changed
 def checkSettings():
     global lastModifiedSettings
     global settings
@@ -84,13 +118,20 @@ def checkSettings():
             settings = json.load(f)
             print("settings reloaded")
 
+# loadIncludedBirds
+# loads inclusion list from text file, names
+# are common name with scientific name in
+# parentheses, line separated
 def loadIncludedBirds():
     inclusions = {}
     with io.open('includedbirds.txt', 'r', encoding='utf-8') as f:
         for line in f.readlines():
+            line = line.strip('\n')
             inclusions[line] = 1
     return inclusions
 
+# checkToken
+# checks token for push notifications
 pushToken = "ExponentPushToken[Blah]"
 lastModifiedToken = 0
 def checkToken():
@@ -117,6 +158,7 @@ def main():
         help='Sets the bounds in which predictions are made. A 4-tuple consisting of the x and y coordinates of\nthe center and the x and y dimensions of the bounds.')
     args = parser.parse_args()
     includedBirds = loadIncludedBirds()
+    print(includedBirds)
     # Annotator renders in software so use a smaller size and scale results
     # for increased performace.
     
@@ -135,13 +177,15 @@ def main():
                     scale_y * (y + height))
         for result in inference.run():
             checkSettings()
-            tresholdForAllFrames = settings["threshold"]*settings["frameCount"]
+            thresholdForAllFrames = settings["threshold"]*settings["frameCount"]
             classes = inaturalist_classification.get_classes(result, top_k=5, threshold=0.1)
             
             print(classes_info(classes))
-            className,classConfidence = isNewVisit(classes,tresholdForAllFrames)
+            className,classConfidence = isNewVisit(classes,thresholdForAllFrames)
             #forceCap = True
-            if (className != None and className in includedBirds):
+            print('className:', className)
+            print(className in includedBirds)
+            if (className in includedBirds):
                 fileName = (className
                 + '*$*' + datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
                 + '*$*' + str(classConfidence)[2:] + '.jpg').replace(' ','-')
